7e3c3155862ce0bdc3258d82933b47d9cf30423b
Tahir <Tahirhussain32145@gmail.com>
2022-09-06 11:58:15 +0545

    Fs Module Completion

---
+++ 9-fs-module.js
@@ -0,0 +1,17 @@
+const {readFileSync, writeFileSync} = require('fs')
+
+const first = readFileSync('./content/first.txt', 'utf8');
+
+const test = readFileSync('./content/subcontent/test.txt', 'utf8');
+
+console.log(first,test);
+
+writeFileSync('./content/result-sync.txt',
+`Here is the result ${first}, ${test} result`,
+
+{flag:'a'}
+)
+//Flag A appendes to the file
+
+
+
